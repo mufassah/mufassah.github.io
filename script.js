eb26002b0e09aae680863c9e16ee20d304e24856
mufassah <austin.davis820@gmail.com>
2016-09-12 21:03:20 -0400

    i did it mom

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  document.getElementById("navMenu").innerHTML =
+
+'<div class="navbar-header">' +
+ '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">' +
+        '<span class="icon-bar"></span>' +
+        '<span class="icon-bar"></span>' +
+        '<span class="icon-bar"></span> ' +
+      '</button>' +
+      '<a class="navbar-brand" href="#" id="header"><b>Heaven eSports</b></a>' +
+    '</div>' +
+    '<div class="collapse navbar-collapse" id="myNavbar">' +
+      '<ul class="nav navbar-nav">' +
+        '<li><a href="index.html">Home</a></li>' +
+        '<li><a href="#">Roster</a></li>' +
+        '<li><a href="teamNews.html">Team News</a></li> ' +
+      '</ul>' +
+    '</div>'
+;
+})
+
