88faef22b14d116a0bcbd1073c6d442c0dab18e4
mufassah <austin.davis820@gmail.com>
2016-09-18 20:21:01 -0400

    crispiest header?

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         '<span class="icon-bar"></span>' +
         '<span class="icon-bar"></span> ' +
       '</button>' +
-      '<a class="navbar-brand" href="#"><img src="heaventemp.PNG" id="header"></a>' +
+      '<a class="navbar-brand" href="#"><img src="navbarHeaven.JPG" id="header"></a>' +
     '</div>' +
     '<div class="collapse navbar-collapse" id="myNavbar">' +
       '<ul class="nav navbar-nav">' +
