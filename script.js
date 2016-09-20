e07360a3e3c35bb5f41534879d96fa3887b1a507
mufassah <austin.davis820@gmail.com>
2016-09-19 22:23:47 -0400

    dasf

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ $(document).ready(function(){
         '<span class="icon-bar"></span>' +
         '<span class="icon-bar"></span> ' +
       '</button>' +
-      '<a class="navbar-brand" href="#"><img src="navbarHeaven.JPG" id="header"></a>' +
+      '<a class="navbar-brand" href="index.html"><img src="navbarHeaven.JPG" id="header"></a>' +
     '</div>' +
     '<div class="collapse navbar-collapse" id="myNavbar">' +
       '<ul class="nav navbar-nav">' +
         '<li><a href="index.html">Home</a></li>' +
         '<li><a href="roster.html">Roster</a></li>' +
         '<li><a href="teamNews.html">News</a></li> ' +
+        '<li><a href="aboutUs.html">About Us</a></li>' +
       '</ul>' +
     '</div>'
 ;
