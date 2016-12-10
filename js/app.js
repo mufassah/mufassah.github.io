9c27438706a82cb3d295102e679da4a1eaca9b52
mufassah <austin.davis820@gmail.com>
2016-12-10 11:54:35 -0500

    ughh

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider){
 	$routeProvider
 	.when("/", {
-		templateUrl : "views/index.html"
+		templateUrl : "views/landing.html"
 	})
 	.when("/roster", {
 		templateUrl : "views/roster.html"
