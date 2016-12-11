62b3b0e4c45c8b05db6f4a2287426222746c467e
mufassah <austin.davis820@gmail.com>
2016-12-10 22:08:35 -0500

    asfd

--- js/shared/script.js
+++ js/shared/script.js
@@ -1,17 +1,12 @@
 
 function connClick() {
     document.getElementById("content").innerHTML = 
-    '<div class="col-md-5 picCenter"><img src="conn2.jpg" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>Con_too_strong</h1><p>My name is Connor and I am 16. I live in San Diego, CA. In my free time I usually play basketball or Destiny ;). At the moment I am trying to grow a Twitch and hopefully that will pan out in the future.</p><br><div class="col-md-6 linkage"><h2>Twitch:</h2><a href="https://twitch.tv/con_too_strong">con_too_strong</a></div><div class="col-md-6 linkage"> <h2>Twitter:</h2><a href="http://twitter.com/con_too_strong">con_too_strong</a></div></div>';
+    '<div class="col-md-5 picCenter"><img src="../images/con-layout.jpg" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>Con_too_strong</h1><p>My name is Connor and I am 16. I live in San Diego, CA. In my free time I usually play basketball or Destiny ;). At the moment I am trying to grow a Twitch and hopefully that will pan out in the future.</p><br><div class="col-md-6 linkage"><h2>Twitch:</h2><a href="https://twitch.tv/con_too_strong">con_too_strong</a></div><div class="col-md-6 linkage"> <h2>Twitter:</h2><a href="http://twitter.com/con_too_strong">con_too_strong</a></div></div>';
 }
 
 function hayesClick(){
   document.getElementById("content").innerHTML = 
-    '<div class="col-md-5 picCenter"><img src="heavenhayes.jpg" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>EliteHayes</h1><p>My name is Zach and I am a 16 year old from beautiful Vancouver, Canada.  I am the leader and founder of Heaven Clan. Stop by and drop a follow and feel free to stay and enjoy my stream! My primary focus is to entertain and interact with my viewers so everybody will have the best experience. Hopefully if you are reading this, I will see you very soon!</p><br><div class="col-md-6 linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/elitehayestv">elitehayestv</a></div><div class="col-md-6 linkage"> <h2>Twitter:</h2><a href="https://twitter.com/elitehayess">elitehayess</a></div></div>';
-}
-
-function legendClick(){
-  document.getElementById("content").innerHTML = 
-    '<div class="col-md-5 picCenter"><img src="legend1.jpg" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>LegendMVL</h1><p class="smallBio">Sum Azn Guy And Player Of Games</p><br><div class="col-md-6 linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/legendmvl">LegendMVL</a></div><div class="col-md-6 linkage"> <h2>Twitter:</h2><a href="https://twitter.com/LegendMVL">LegendMVL</a></div><div class="col-md-12 linkage"><h2>YouTube:</h2><a href="https://www.youtube.com/channel/UCV53MdNKLY45QJLfql2w7qw">LegendMvl</a></div></div>';
+    '<div class="col-md-5 picCenter"><img src="../images/hayes-layout.jpg" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>EliteHayes</h1><p>My name is Zach and I am a 16 year old from beautiful Vancouver, Canada.  I am the leader and founder of Heaven Clan. Stop by and drop a follow and feel free to stay and enjoy my stream! My primary focus is to entertain and interact with my viewers so everybody will have the best experience. Hopefully if you are reading this, I will see you very soon!</p><br><div class="col-md-6 linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/elitehayestv">elitehayestv</a></div><div class="col-md-6 linkage"> <h2>Twitter:</h2><a href="https://twitter.com/elitehayess">elitehayess</a></div></div>';
 }
 
 function genesisClick(){
@@ -21,9 +16,8 @@ function genesisClick(){
 
 function snuggsClick(){
   document.getElementById("content").innerHTML = 
-    '<div class="col-md-5 picCenter"><img src="snuggsAvi.jpg" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>Snuggs</h1><p>Hi, I am Jamie. 25 years old, hopeless Year 1 Destiny player, journalist, Halo veteran, and co-founder of Heaven.</p><br><div class="col-md-4 linkage"><h2>Twitter:</h2><a href="https://twitter.com/Snuggs__">Snuggs__</a></div><div class="col-md-8 linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/snuggs__">Snuggs__</a></div></div>';
+    '<div class="col-md-5 picCenter"><img src="../images/snuggs-layout.jpg" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>Snuggs</h1><p>Hi, I am Jamie. 25 years old, hopeless Year 1 Destiny player, journalist, Halo veteran, and co-founder of Heaven.</p><br><div class="col-md-4 linkage"><h2>Twitter:</h2><a href="https://twitter.com/Snuggs__">Snuggs__</a></div><div class="col-md-8 linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/snuggs__">Snuggs__</a></div></div>';
 }
-
 
 function cynClick(){
   document.getElementById("content").innerHTML = 
@@ -33,10 +27,7 @@ function mufassahClick(){
    document.getElementById("content").innerHTML = 
     '<div class="col-md-5 picCenter"><img src="mufassahAvi.JPG" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>Mufassah</h1><p class="smallBio">Hello good sirs, my name is Austin but probably better known as Mufassah. I am 22 years old and am a player and the web designer for Heaven Clan. I enjoy the competitive spirit of video games and look forward to what this game and clan can bring to the future of eSports!</p><br><div class="col-md-6 linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/mufassah">Mufassah</a></div><div class="col-md-6 linkage"> <h2>Twitter:</h2><a href="https://twitter.com/MufassahTV">MufassahTV</a></div>';
 }
-function zaeClick(){
-   document.getElementById("content").innerHTML = 
-    '<div class="col-md-5 picCenter"><img src="hayestemp2.JPG" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>Zae</h1><p class="smallBio">My name is Jose but you can call me Zae. I am 16 and currently living in Florida. I have been gaming since Halo 3. My goal is to have fun.</p></div>';
-}
+
 function misaClick(){
    document.getElementById("content").innerHTML = 
     '<div class="col-md-5 picCenter"><img src="qguard.jpg" class="img-circle img-responsive mainBio"></div><div class="col-md-1"></div><div class="col-md-6" id="bio"><h1>MisaOP</h1><p class="smallBio">My name is Misa, I am a 17 year old kid who plays destiny and various other games. I am always making jokes or saying something goofy. </p><br><div class="col-md-6 linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/Mesahh">Mesahh</a></div><div class="col-md-6 linkage"> <h2>Twitter:</h2><a href="https://twitter.com/MisaWombo">MisaWombo</a></div>';
