456974537b50ea9abfda1d9e44b23532a35c8dff
mufassah <austin.davis820@gmail.com>
2016-12-11 01:58:43 -0500

    52

--- js/shared/script.js
+++ js/shared/script.js
@@ -1,36 +1,36 @@
 
 function connClick() {
     document.getElementById("content").innerHTML = 
-    '<div class="picCenter"><img src="../images/con-layout.jpg" class="img-responsive mainBio"></div><div id="bio"><h1>Con_too_strong</h1><p>My name is Connor and I am 16. I live in San Diego, CA. In my free time I usually play basketball or Destiny ;). At the moment I am trying to grow a Twitch and hopefully that will pan out in the future.</p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://twitch.tv/con_too_strong">con_too_strong</a></div><div class="linkage"> <h2>Twitter:</h2><a href="http://twitter.com/con_too_strong">con_too_strong</a></div></div></div>';
+    '<div class="picCenter"><img src="../images/con-layout.jpg" class="img-responsive mainBio"></div><div id="bio" class="transbox"><h1>Con_too_strong</h1><p>My name is Connor and I am 16. I live in San Diego, CA. In my free time I usually play basketball or Destiny ;). At the moment I am trying to grow a Twitch and hopefully that will pan out in the future.</p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://twitch.tv/con_too_strong">con_too_strong</a></div><div class="linkage"> <h2>Twitter:</h2><a href="http://twitter.com/con_too_strong">con_too_strong</a></div></div></div>';
 }
 
 function hayesClick(){
   document.getElementById("content").innerHTML = 
-    '<div class="picCenter"><img src="../images/hayes-layout.jpg" class="img-responsive mainBio"></div><div id="bio"><h1>EliteHayes</h1><p>My name is Zach and I am a 16 year old from beautiful Vancouver, Canada.  I am the leader and founder of Heaven Clan. Stop by and drop a follow and feel free to stay and enjoy my stream! My primary focus is to entertain and interact with my viewers so everybody will have the best experience. Hopefully if you are reading this, I will see you very soon!</p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/elitehayestv">elitehayestv</a></div><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/elitehayess">elitehayess</a></div></div></div>';
+    '<div class="picCenter"><img src="../images/hayes-layout.jpg" class="img-responsive mainBio"></div><div id="bio" class="transbox"><h1>EliteHayes</h1><p>My name is Zach and I am a 16 year old from beautiful Vancouver, Canada.  I am the leader and founder of Heaven Clan. Stop by and drop a follow and feel free to stay and enjoy my stream! My primary focus is to entertain and interact with my viewers so everybody will have the best experience. Hopefully if you are reading this, I will see you very soon!</p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/elitehayestv">elitehayestv</a></div><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/elitehayess">elitehayess</a></div></div></div>';
 }
 
 function genesisClick(){
   document.getElementById("content").innerHTML = 
-    '<div class="picCenter"><img src="genesis2.jpg" class="img-resopnsive mainBio"></div><div id="bio"><h1>G3NE5iS</h1><p>Hey! My name is Luc but I am mostly known as G3NE5iS, I am 18 and I design stuff...I have been designing for around 6 months but I have still got loads to learn and I hope to improve a bunch! I am super happy to be part of such a positive group like Heaven and I am sure this experience will help me grow as a person and a designer.</p><div class="links-container"><div class="linkage"><h2>Twitter:</h2><a href="https://twitter.com/iG3NE5iS">iG3NE5iS</a></div><div class="linkage"><h2>Personal Website:</h2><a href="http://g3ne5is.wixsite.com/designer">G3NE5iS Graphic Designs</a></div></div></div>';
+    '<div class="picCenter"><img src="genesis2.jpg" class="img-resopnsive mainBio"></div><div id="bio" class="transbox"><h1>G3NE5iS</h1><p>Hey! My name is Luc but I am mostly known as G3NE5iS, I am 18 and I design stuff...I have been designing for around 6 months but I have still got loads to learn and I hope to improve a bunch! I am super happy to be part of such a positive group like Heaven and I am sure this experience will help me grow as a person and a designer.</p><div class="links-container"><div class="linkage"><h2>Twitter:</h2><a href="https://twitter.com/iG3NE5iS">iG3NE5iS</a></div><div class="linkage"><h2>Personal Website:</h2><a href="http://g3ne5is.wixsite.com/designer">G3NE5iS Graphic Designs</a></div></div></div>';
 }
 
 function snuggsClick(){
   document.getElementById("content").innerHTML = 
-    '<div class="picCenter"><img src="../images/snuggs-layout.jpg" class="img-responsive mainBio"></div><div id="bio"><h1>Snuggs</h1><p>Hi, I am Jamie. 25 years old, hopeless Year 1 Destiny player, journalist, Halo veteran, and co-founder of Heaven.</p><div class="links-container"><div class="linkage"><h2>Twitter:</h2><a href="https://twitter.com/Snuggs__">Snuggs__</a></div><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/snuggs__">Snuggs__</a></div></div></div>';
+    '<div class="picCenter"><img src="../images/snuggs-layout.jpg" class="img-responsive mainBio"></div><div id="bio" class="transbox"><h1>Snuggs</h1><p>Hi, I am Jamie. 25 years old, hopeless Year 1 Destiny player, journalist, Halo veteran, and co-founder of Heaven.</p><div class="links-container"><div class="linkage"><h2>Twitter:</h2><a href="https://twitter.com/Snuggs__">Snuggs__</a></div><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/snuggs__">Snuggs__</a></div></div></div>';
 }
 
 function cynClick(){
   document.getElementById("content").innerHTML = 
-    '<div class="picCenter"><img src="cyntemp.jpg" class="img-responsive mainBio"></div><div id="bio"><h1>CynKZBH</h1><p class="smallBio">Cinema 4D Designs</p><div class="links-container"><div class="linkage"><h2>Twitter:</h2><a href="https://twitter.com/CynKZBH">CynKZBH</a></div></div></div>';
+    '<div class="picCenter"><img src="cyntemp.jpg" class="img-responsive mainBio"></div><div id="bio" class="transbox"><h1>CynKZBH</h1><p class="smallBio">Cinema 4D Designs</p><div class="links-container"><div class="linkage"><h2>Twitter:</h2><a href="https://twitter.com/CynKZBH">CynKZBH</a></div></div></div>';
 }
 function mufassahClick(){
    document.getElementById("content").innerHTML = 
-    '<div class="picCenter"><img src="mufassahAvi.JPG" class="img-responsive mainBio"></div><div id="bio"><h1>Mufassah</h1><p class="smallBio">Hello good sirs, my name is Austin but probably better known as Mufassah. I am 22 years old and am a player and the web designer for Heaven Clan. I enjoy the competitive spirit of video games and look forward to what this game and clan can bring to the future of eSports!</p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/mufassah">Mufassah</a></div><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/MufassahTV">MufassahTV</a></div></div></div>';
+    '<div class="picCenter"><img src="mufassahAvi.JPG" class="img-responsive mainBio"></div><div id="bio" class="transbox"><h1>Mufassah</h1><p class="smallBio">Hello good sirs, my name is Austin but probably better known as Mufassah. I am 22 years old and am a player and the web designer for Heaven Clan. I enjoy the competitive spirit of video games and look forward to what this game and clan can bring to the future of eSports!</p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/mufassah">Mufassah</a></div><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/MufassahTV">MufassahTV</a></div></div></div>';
 }
 
 function misaClick(){
    document.getElementById("content").innerHTML = 
-    '<div class="picCenter"><img src="qguard.jpg" class="img-responsive mainBio"></div><div id="bio"><h1>MisaOP</h1><p class="smallBio">My name is Misa, I am a 17 year old kid who plays destiny and various other games. I am always making jokes or saying something goofy. </p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/Mesahh">Mesahh</a></div><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/MisaWombo">MisaWombo</a></div></div></div>';
+    '<div class="picCenter"><img src="qguard.jpg" class="img-responsive mainBio"></div><div id="bio" class="transbox"><h1>MisaOP</h1><p class="smallBio">My name is Misa, I am a 17 year old kid who plays destiny and various other games. I am always making jokes or saying something goofy. </p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/Mesahh">Mesahh</a></div><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/MisaWombo">MisaWombo</a></div></div></div>';
 }
 
 
