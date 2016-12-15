88ca518d14a34d8be1978ba7fa66d441fccf7ffc
mufassah <austin.davis820@gmail.com>
2016-12-14 20:31:22 -0500

    fsd

--- js/controllers/AboutUsController.js
+++
@@ -1,2 +0,0 @@
-app.controller('AboutUsController', ['$scope', function($scope) {
-}]);
---
+++ js/controllers/QuizController4.js
@@ -0,0 +1,28 @@
+app.controller('QuizController4', ['$scope', '$http', function($scope, $http) {
+	$scope.totalPoints = 0;
+	$http.get('js/quiz4_data.json').then(function(quizData){
+	$scope.myQuestions = quizData.data;
+	$scope.totalQuestions = $scope.myQuestions.length;
+	});
+	$scope.isNextEnabled = "notEnabled";
+	$scope.enable = function(qIndex, aIndex){
+		$scope.isNextEnabled = "";
+		$scope.className = "answer-" + qIndex + "-" + aIndex;
+		$scope.qIndex = qIndex;
+		$scope.aIndex = aIndex;
+	};
+	$scope.disableMe = function() {
+		$scope.className = "";
+		$scope.totalPoints += $scope.myQuestions[$scope.qIndex].answers[$scope.aIndex].points;
+	};
+	$scope.submit = function() {
+		$scope.totalPoints += $scope.myQuestions[$scope.qIndex].answers[$scope.aIndex].points;
+		console.log($scope.totalPoints);
+		if($scope.totalPoints < 8)
+			$scope.resultsLink = "#aboutUs";
+		else if($scope.totalPoints < 15)
+			$scope.resultsLink = "#roster";
+		else
+			$scope.resultsLink = "#quiz2";
+	};
+}]);
