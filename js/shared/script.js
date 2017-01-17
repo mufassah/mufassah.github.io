8fc3a2316269033e2b436235368f3113eb5566eb
mufassah <austin.davis820@gmail.com>
2017-01-17 13:44:15 -0500

    wefwef

--- js/shared/script.js
+++ js/shared/script.js
@@ -1,17 +1,7 @@
 
-function connClick() {
-    document.getElementById("content").innerHTML = 
-    '<div id="bio" class="transbox"><div class="picCenter"><img src="../images/con-layout.jpg" class="img-responsive mainBio"></div><div class="bio-div"><div class="bio-p"><p>My name is Connor and I am 16. I live in San Diego, CA. In my free time I usually play basketball or Destiny ;). At the moment I am trying to grow a Twitch and hopefully that will pan out in the future.</p></div><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://twitch.tv/con_too_strong">con_too_strong</a></div><div class="linkage"> <h2>Twitter:</h2><a href="http://twitter.com/con_too_strong">con_too_strong</a></div></div></div></div>';
-}
-
 function hayesClick(){
  document.getElementById("content").innerHTML = 
   '<div id="bio" class="transbox"><div class="picCenter"><img src="../images/hayes-header.jpg" class="mainBio img-responsive"></div><div class="bio-div"><div class="bio-p"><img src="../images/hayes-bio.jpg" class="img-responsive"></div><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/elitehayestv">elitehayestv</a></div><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/elitehayess">elitehayess</a></div></div></div></div>';
-}
-
-function genesisClick(){
-  document.getElementById("content").innerHTML = 
-    '<div class="picCenter"><img src="genesis2.jpg" class="img-resopnsive mainBio"></div><div id="bio" class="transbox"><h1>G3NE5iS</h1><p>Hey! My name is Luc but I am mostly known as G3NE5iS, I am 18 and I design stuff...I have been designing for around 6 months but I have still got loads to learn and I hope to improve a bunch! I am super happy to be part of such a positive group like Heaven and I am sure this experience will help me grow as a person and a designer.</p><div class="links-container"><div class="linkage"><h2>Twitter:</h2><a href="https://twitter.com/iG3NE5iS">iG3NE5iS</a></div><div class="linkage"><h2>Personal Website:</h2><a href="http://g3ne5is.wixsite.com/designer">G3NE5iS Graphic Designs</a></div></div></div>';
 }
 
 function snuggsClick(){
@@ -26,11 +16,6 @@ function cynClick(){
 function mufassahClick(){
    document.getElementById("content").innerHTML = 
     '<div id="bio" class="transbox"><div class="picCenter"><img src="../images/mufassah-header.jpg" class="img-responsive mainBio"></div><div class="bio-div"><div class="bio-p"><p>Hello good sirs, my name is Austin but probably better known as Mufassah. I am 22 years old and am a player and the web designer for Heaven Clan. I enjoy the competitive spirit of video games and look forward to what this game and clan can bring to the future of eSports!</p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/mufassah">Mufassah</a></div><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/MufassahTV">MufassahTV</a></div></div></div></div>';
-}
-
-function misaClick(){
-   document.getElementById("content").innerHTML = 
-    '<div class="picCenter"><img src="qguard.jpg" class="img-responsive mainBio"></div><div id="bio" class="transbox"><h1>MisaOP</h1><p class="smallBio">My name is Misa, I am a 17 year old kid who plays destiny and various other games. I am always making jokes or saying something goofy. </p><div class="links-container"><div class="linkage"><h2>Twitch:</h2><a href="https://www.twitch.tv/Mesahh">Mesahh</a></div><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/MisaWombo">MisaWombo</a></div></div></div>';
 }
 
 function heavenClick(){
@@ -54,7 +39,22 @@ function swiftClick(){
    document.getElementById("content").innerHTML = 
     '<div id="bio" class="transbox"><div class="picCenter"><img src="../images/swift-header.jpg" class="mainBio img-responsive"></div><div class="bio-div"><div class="bio-p"><p>Swift needs to give me info.</p></div><div class="links-container"><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/HeavenClan_">Heaven Clan</a></div></div></div></div>';
 }
-function swiftClick(){
+function iceClick(){
    document.getElementById("content").innerHTML = 
     '<div id="bio" class="transbox"><div class="picCenter"><img src="../images/ice-header.jpg" class="mainBio img-responsive"></div><div class="bio-div"><div class="bio-p"><p>Ice needs to give me info.</p></div><div class="links-container"><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/HeavenClan_">Heaven Clan</a></div></div></div></div>';
+}
+
+function infancyClick(){
+   document.getElementById("content").innerHTML = 
+    '<div id="bio" class="transbox"><div class="picCenter"><img src="../images/ice-header.jpg" class="mainBio img-responsive"></div><div class="bio-div"><div class="bio-p"><p>Infancy needs to give me info.</p></div><div class="links-container"><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/HeavenClan_">Heaven Clan</a></div></div></div></div>';
+}function legendClick(){
+   document.getElementById("content").innerHTML = 
+    '<div id="bio" class="transbox"><div class="picCenter"><img src="../images/legend-header.jpg" class="mainBio img-responsive"></div><div class="bio-div"><div class="bio-p"><p>Legend needs to give me info.</p></div><div class="links-container"><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/HeavenClan_">Heaven Clan</a></div></div></div></div>';
+}function zaeClick(){
+   document.getElementById("content").innerHTML = 
+    '<div id="bio" class="transbox"><div class="picCenter"><img src="../images/zae-header.jpg" class="mainBio img-responsive"></div><div class="bio-div"><div class="bio-p"><p>ZaE needs to give me info.</p></div><div class="links-container"><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/HeavenClan_">Heaven Clan</a></div></div></div></div>';
+}
+function jbizClick(){
+   document.getElementById("content").innerHTML = 
+    '<div id="bio" class="transbox"><div class="picCenter"><img src="../images/ice-header.jpg" class="mainBio img-responsive"></div><div class="bio-div"><div class="bio-p"><p>Jbiz needs to give me info.</p></div><div class="links-container"><div class="linkage"> <h2>Twitter:</h2><a href="https://twitter.com/HeavenClan_">Heaven Clan</a></div></div></div></div>';
 }
